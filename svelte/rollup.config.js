6cb002b3d5003a440271182462522e7034996747
sunshineplan <sunshineplan@users.noreply.github.com>
2020-12-21 22:08:50 +0800

    Update

--- svelte/rollup.config.js
+++ svelte/rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import del from 'rollup-plugin-delete';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -33,12 +34,13 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		del({ targets: 'public/build/*' }),
 		svelte({
 			preprocess: sveltePreprocess(),
 			compilerOptions: {

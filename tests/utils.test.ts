01a42ef4cb82af46173b0fb3f4496847539257ce
sunshineplan <sunshineplan@users.noreply.github.com>
2025-06-24 13:47:10 +0800

    Update utils.test.ts

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import utils, { format, preprocess } from '../src/utils'
+import utils, { format } from '../src/utils'
 
 test('Contains', () => {
     expect(utils.contains(['1', '2', '3'], '1')).toBeTruthy()
@@ -9,11 +9,6 @@ test('Contains', () => {
 test('Range', () => expect(utils.range('2', '5')).toStrictEqual(['2', '3', '4', '5']))
 
 test('Sort', () => expect(utils.sort(['10', '2', '1', 'a'])).toStrictEqual(['1', '2', '10', 'a']))
-
-test('Preprocess', () => {
-    expect(preprocess('3\n1\n\n\n2\na\n\n\n')).toStrictEqual(['3', '1', '2', 'a'])
-    expect(preprocess('3\n"1\n\n\n2"\n"a"\n\n\n')).toStrictEqual(['3', '1', '2', '"a"'])
-})
 
 test('Format', () => {
     expect(format(1, ['1'])).toBe('\nTotal 1 record\n\nresult:\n1')
